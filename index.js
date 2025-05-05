40e746a50a951be399be25a88feb5d3b21d31f14
Nad Does Code <nadirbek631@gmail.com>
2025-05-05 11:34:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+import { next } from '@vercel/edge';
+
+export default function middleware(req) {
+  return next({
+    headers: {
+      'Referrer-Policy': 'origin-when-cross-origin',
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      'X-DNS-Prefetch-Control': 'on',
+      'Strict-Transport-Security':
+        'max-age=31536000; includeSubDomains; preload',
+    },
+  });
+}
+
+
 const todoForm = document.getElementById("form");
 const todoInput = document.getElementById("input");
 const listContainer = document.getElementById("todo-ul");
